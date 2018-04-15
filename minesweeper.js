2859ec9fab75960b383fcb3a85b79d6e16a21ae4
unknown <indigogeorgiana@gmail.com>
2018-04-15 07:15:57 +0100

    "completed 6.2 Minesweeper: counting the boom"

--- minesweeper.js
+++ minesweeper.js
@@ -3,50 +3,93 @@ document.addEventListener('DOMContentLoaded', startGame)
 // Define your `board` object here!
  var board = { 
   cells: [
-    {row: 0, col: 0, isMine:'', hidden:!true}, 
-    {row: 0, col: 1, isMine:'', hidden:!true}, 
-    {row: 0, col: 2, isMine:'', hidden:!true}, 
-    {row: 0, col: 3, isMine:'', hidden:!true},
-    {row: 1, col: 0, isMine:'', hidden:!true},
-    {row: 1, col: 1, isMine:'', hidden:!true}, 
-    {row: 1, col: 2, isMine:'', hidden:!true}, 
-    {row: 1, col: 3, isMine:'', hidden:!true},
-    {row: 2, col: 0, isMine:'', hidden:!true}, 
-    {row: 2, col: 1, isMine:'', hidden:!true}, 
-    {row: 2, col: 2, isMine:'', hidden:!true}, 
-    {row: 2, col: 3, isMine:'', hidden:!true},
-    {row: 3, col: 0, isMine:'', hidden:!true},
-    {row: 3, col: 1, isMine:'', hidden:!true}, 
-    {row: 3, col: 2, isMine:'', hidden:!true}, 
-    {row: 3, col: 3, isMine:'', hidden:!true},
+  //Row Zero
+    {row: 0, col: 0, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 0, col: 1, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 0, col: 2, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 0, col: 3, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+    {row: 0, col: 4, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 0, col: 5, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+  //Row One
+    {row: 1, col: 0, isMine:'', isMarked: "", hidden:!true , surroundingMines:''},
+    {row: 1, col: 1, isMine:'', isMarked: "", hidden:!true , surroundingMines:''}, 
+    {row: 1, col: 2, isMine:'', isMarked: "", hidden:!true , surroundingMines:''}, 
+    {row: 1, col: 3, isMine:'', isMarked: "", hidden:!true , surroundingMines:''},
+    {row: 1, col: 4, isMine:'', isMarked: "", hidden:!true , surroundingMines:''}, 
+    {row: 1, col: 5, isMine:'', isMarked: "", hidden:!true , surroundingMines:''},
+  //Row Two
+    {row: 2, col: 0, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 2, col: 1, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 2, col: 2, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 2, col: 3, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+    {row: 2, col: 4, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 2, col: 5, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+  //Row Three
+    {row: 3, col: 0, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+    {row: 3, col: 1, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 3, col: 2, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 3, col: 3, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+    {row: 3, col: 4, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 3, col: 5, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+  //Row Four
+    {row: 4, col: 0, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+    {row: 4, col: 1, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 4, col: 2, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 4, col: 3, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+    {row: 4, col: 4, isMine:'', isMarked: "", hidden:!true, surroundingMines:''}, 
+    {row: 4, col: 5, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+  //Row Five
+    {row: 5, col: 0, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+    {row: 5, col: 1, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+    {row: 5, col: 2, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+    {row: 5, col: 3, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+    {row: 5, col: 4, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
+    {row: 5, col: 5, isMine:'', isMarked: "", hidden:!true, surroundingMines:''},
     ]
  }
   
-
+// Don't remove this function call: it makes the game work!
 function startGame () {
-  // Don't remove this function call: it makes the game work!
+// Write a for loop that loops through the contents of board.cells.
+for (var i = 0; i <board.cells.length; i++){
+//You'll need to pass each cell as an argument, call countSurroundingMinesfor each cell in board.cells. 
+//Assign the result of countSurroundingMines to a property on each cell object. The new property 
+//should be called surroundingMines
+board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+ }
   lib.initBoard()
 }
-
 // Define this function to look for a win condition:
-//
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
-
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+  for (var i = 0; i < board.cells.length; i++) {
+    var check = board.cells[i];
+    if (check.isMine && !check.isMarked){
+      return;
+    }
+    else if (!check.isMine && check.hidden) {
+      return;
+    } 
+  }
+// You can use this function call to declare a winner (once you've
+// detected that they've won, that is!)
+  lib.displayMessage('You win!')
 }
 
+  function countSurroundingMines (cell) {
+//Define functions it so it returns the number of cells 
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col);
+  var count = 0;
+  for (var i = 0; i < surrounding.length; i++){
+//around the current cell that have the isMine property set to true 
+    if (cell.isMine[i] == true){
 // Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
-//
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-//
-// It will return cell objects in an array. You should loop through 
-// them, counting the number of times `cell.isMine` is true.
-function countSurroundingMines (cell) {
-}
-
+// (there could be as many as 8).       
+    count++;
+    }
+  }
+  // It will return cell objects in an array, You should loop through
+  //them, counting the number of times `cell.isMine` is true.
+    return count;
+  }
